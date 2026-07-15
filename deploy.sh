aeba9679d75077754f116733c05e84e1097e1dc5
hex2rgb <robert196@163.com>
2026-07-15 12:07:46 +0800

    fix: 更新
    - modified eploy.sh
    -  docker-compose.yml
    -  docker-compose-local.yml

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-docker compose down --remove-orphans
-docker compose up -d --build
+docker compose -f docker-compose-local.yml down --remove-orphans
+docker compose -f docker-compose-local.yml up -d --build
